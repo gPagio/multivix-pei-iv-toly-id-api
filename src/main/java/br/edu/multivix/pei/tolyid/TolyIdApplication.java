c0c18a2ec0d73b128673b0bf6ce452dc9e955b44
gPagio <guilhermepagio.contato@gmail.com>
2024-09-18 01:31:59 -0300

    feat: first commit

---
+++ src/main/java/br/edu/multivix/pei/tolyid/TolyIdApplication.java
@@ -0,0 +1,13 @@
+package br.edu.multivix.pei.tolyid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolyIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TolyIdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/multivix/pei/tolyid/TolyIdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.multivix.pei.tolyid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TolyIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
