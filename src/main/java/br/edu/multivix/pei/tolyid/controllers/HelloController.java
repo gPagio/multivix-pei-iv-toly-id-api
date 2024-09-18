5ed6f063adbcbf8e8a0ff0559b9563532112355f
gPagio <guilhermepagio.contato@gmail.com>
2024-09-18 01:44:38 -0300

    feat: controller teste

---
+++ src/main/java/br/edu/multivix/pei/tolyid/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package br.edu.multivix.pei.tolyid.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello(){
+        return "Oi meu chapa!";
+    }
+
+}
