2f7160ac755cb1c8e6a5a3524edafece65fb2fc3
gPagio <guilhermepagio.contato@gmail.com>
2024-10-05 23:31:25 -0300

    fix: remove id do tatu
    Remove campo id do tatu do DTO, o mesmo deve vir no path da requisicao

--- src/main/java/br/edu/multivix/pei/tolyid/domain/captura/DadosCadastroCapturaDTO.java
+++ src/main/java/br/edu/multivix/pei/tolyid/domain/captura/DadosCadastroCapturaDTO.java
@@ -6,16 +6,14 @@ import br.edu.multivix.pei.tolyid.domain.dadosgerais.DadosCadastroDadosGeraisDTO
 import br.edu.multivix.pei.tolyid.domain.fichaanestesica.DadosCadastroFichaAnestesicaDTO;
 import jakarta.validation.constraints.NotNull;
 
-public record DadosCadastroCapturaDTO(Long idTatu,
-  
-                                    @NotNull(message = "É obrigatório o preenchimento dos dados gerais para o cadastro de uma captura!")
-                                    DadosCadastroDadosGeraisDTO dadosGerais,
+public record DadosCadastroCapturaDTO(  @NotNull(message = "É obrigatório o preenchimento dos dados gerais para o cadastro de uma captura!")
+                                        DadosCadastroDadosGeraisDTO dadosGerais,
 
-                                    @NotNull(message = "É obrigatório o preenchimento da ficha anestésica para o adastro de uma captura!")
-                                    DadosCadastroFichaAnestesicaDTO fichaAnestesica,
+                                        @NotNull(message = "É obrigatório o preenchimento da ficha anestésica para o adastro de uma captura!")
+                                        DadosCadastroFichaAnestesicaDTO fichaAnestesica,
 
-                                    @NotNull(message = "É obrigatório o preenchimento da biometria para o cadastro de uma captura!")
-                                    DadosCadastroBiometriaDTO biometria,
+                                        @NotNull(message = "É obrigatório o preenchimento da biometria para o cadastro de uma captura!")
+                                        DadosCadastroBiometriaDTO biometria,
 
-                                    @NotNull(message = "É obrigatório o preenchimento da amostra para o cadastro de uma captura!")
-                                    DadosCadastroAmostraDTO amostra) { }
+                                        @NotNull(message = "É obrigatório o preenchimento da amostra para o cadastro de uma captura!")
+                                        DadosCadastroAmostraDTO amostra) { }
