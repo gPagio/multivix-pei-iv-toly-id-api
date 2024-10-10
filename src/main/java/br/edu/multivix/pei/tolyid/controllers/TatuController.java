a7ddeb0d55ae08924b2f4b541fcb168d46d6ac4e
gPagio <guilhermepagio.contato@gmail.com>
2024-10-10 15:47:33 -0300

    feat: listar tatu por identificacao animal

--- src/main/java/br/edu/multivix/pei/tolyid/controllers/TatuController.java
+++ src/main/java/br/edu/multivix/pei/tolyid/controllers/TatuController.java
@@ -3,6 +3,8 @@ package br.edu.multivix.pei.tolyid.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import br.edu.multivix.pei.tolyid.domain.tatu.TatuService;
 import br.edu.multivix.pei.tolyid.domain.tatu.dto.DadosCadastroTatuDTO;
 import jakarta.validation.Valid;
 
+@SuppressWarnings("rawtypes")
 @RestController
 @RequestMapping("/tatus")
 public class TatuController {
@@ -19,11 +22,16 @@ public class TatuController {
     @Autowired
     private TatuService tatuService;
 
-    @SuppressWarnings("rawtypes")
     @PostMapping(path = "/cadastrar")
     @Transactional
     public ResponseEntity cadastraTatu(@RequestBody @Valid DadosCadastroTatuDTO dados){
         var dadosListagemTatuDTO = tatuService.cadastraTatu(dados);
+        return ResponseEntity.ok(dadosListagemTatuDTO);
+    }
+
+    @GetMapping(path = "/listar/{identificacaoAnimal}")
+    public ResponseEntity listaTatuPorIdentificacaoAnimal(@PathVariable String identificacaoAnimal){
+        var dadosListagemTatuDTO = tatuService.listaTatuPorIdentificacaoAnimal(identificacaoAnimal);
         return ResponseEntity.ok(dadosListagemTatuDTO);
     }
     
--- src/main/java/br/edu/multivix/pei/tolyid/domain/tatu/TatuRepository.java
+++ src/main/java/br/edu/multivix/pei/tolyid/domain/tatu/TatuRepository.java
@@ -1,9 +1,13 @@
 package br.edu.multivix.pei.tolyid.domain.tatu;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TatuRepository extends JpaRepository<Tatu, Long>{
     Boolean existsByIdentificacaoAnimal(String identificacao);
 
     Boolean existsByNumeroMicrochip(Integer numeroMicrochip);
+
+    Optional<Tatu> findByIdentificacaoAnimal(String identificacaoAnimal);
 }
