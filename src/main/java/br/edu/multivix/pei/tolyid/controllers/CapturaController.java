e19f272d3ed89d475b42bdbb0410d7c493547bcb
gPagio <guilhermepagio.contato@gmail.com>
2024-10-23 19:55:33 -0300

    fix: metodo http controller captura
    Correcao do metodo http no metodo de atualizar capturas

--- src/main/java/br/edu/multivix/pei/tolyid/controllers/CapturaController.java
+++ src/main/java/br/edu/multivix/pei/tolyid/controllers/CapturaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -59,7 +60,7 @@ public class CapturaController {
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping(path = "/atualizar/{id}")
+    @PutMapping(path = "/atualizar/{id}")
     public ResponseEntity atualizaCapturaPorId(@PathVariable Long id, @RequestBody DadosAtualizacaoCapturaDTO dados){
         return null;
     }
