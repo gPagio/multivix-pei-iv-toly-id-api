9d8fe42fe95b37c65f7c5b591e3838471eecd016
Guilherme Pagio <116508581+gPagio@users.noreply.github.com>
2024-11-12 11:15:23 -0300

    revert: remocao da secao schema do swagger ui
    No commit [b2a50a8f3d853346c51b17a7eee5b1b31f68caa2] a secao sechema foi removida do swagger ui, estamos revertendo esse commmit

--- src/main/java/br/edu/multivix/pei/tolyid/domain/captura/dto/DadosAtualizacaoCapturaDTO.java
+++ src/main/java/br/edu/multivix/pei/tolyid/domain/captura/dto/DadosAtualizacaoCapturaDTO.java
@@ -4,10 +4,8 @@ import br.edu.multivix.pei.tolyid.domain.amostra.dto.DadosAtualizacaoAmostraDTO;
 import br.edu.multivix.pei.tolyid.domain.biometria.dto.DadosAtualizacaoBiometriaDTO;
 import br.edu.multivix.pei.tolyid.domain.dadosgerais.dto.DadosAtualizacaoDadosGeraisDTO;
 import br.edu.multivix.pei.tolyid.domain.fichaanestesica.dto.DadosAtualizacaoFichaAnestesicaDTO;
-import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.validation.Valid;
 
-@Hidden
 public record DadosAtualizacaoCapturaDTO(DadosAtualizacaoDadosGeraisDTO dadosGerais,
 
                                         @Valid
--- src/main/java/br/edu/multivix/pei/tolyid/domain/tatu/dto/DadosCadastroTatuDTO.java
+++ src/main/java/br/edu/multivix/pei/tolyid/domain/tatu/dto/DadosCadastroTatuDTO.java
@@ -1,9 +1,7 @@
 package br.edu.multivix.pei.tolyid.domain.tatu.dto;
 
-import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.validation.constraints.NotNull;
 
-@Hidden
 public record DadosCadastroTatuDTO (@NotNull(message = "A identificação do animal deve ser informada para realizar seu cadastro!")
                                     String identificacaoAnimal,
                                     Integer numeroMicrochip){ }
